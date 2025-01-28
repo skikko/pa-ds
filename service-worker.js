946647bb3703467f2286cc9ef7dc55e75c4a3af5
Sk1kko <francescolaterza@gmail.com>
2025-01-28 09:47:22 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,26 @@
 const CACHE_NAME = 'photoapp2-v1';
 const ASSETS = [
-    '/',
-    '/index.html',
-    '/assets/css/styles.css',
-    '/assets/js/main.js',
-    '/assets/images/default-frame.png'
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/demo.png',
+  '/camera-icon.png',
+  '/frame-icon.png',
+  '/download-icon.png',
+  '/pwa-icon.png',
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => response || fetch(event.request))
-    );
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
 });
